fdc5fce02be675d35c3748f3b74264d2dce7986a
luis <yongchun1683@gmail.com>
2019-01-30 16:04:03 +0800

    the registered user data can now store in firestore

--- app/src/app/register/register.page.ts
+++ app/src/app/register/register.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { AlertController } from '@ionic/angular';
+
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 @Component({
@@ -14,23 +18,56 @@ export class RegisterPage implements OnInit {
   username: string;
   password: string;
   cpassword: string;
+  fullname: string;
+  mremarks: string;
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth,
+              public afStore: AngularFirestore,
+              public alert: AlertController,
+              public router: Router
+              ) { }
 
   ngOnInit() {
   }
 
   register() {
-    const { username, password, cpassword } = this;
+    const { username, password, cpassword, fullname, mremarks } = this;
+    if(password != cpassword) {
+      this.showAlert("Error!", "Password don't match!"); 
+      return console.log("Password don't match!");
+    }
+
     try {
       this.afAuth.auth.createUserWithEmailAndPassword(username, password).then(cred => {
-        console.log(cred);
+        const userId = cred.user.uid;
+        const email = cred.user.email;
+        const displayName = cred.user.displayName;
+
+        const user = {
+          userid: userId,
+          useremail: email,
+          userfullname: fullname,
+          usermremarks: mremarks
+        }
+
+        this.afStore.collection('users').doc(userId).set(user);
+        this.showAlert("Success!", "Welcome onboard!");
+        this.router.navigate(['/login']);
       });
-      console.log('success');
 
     } catch(err) {
-      console.dir(err);
+      this.showAlert("Error!", err.message);
     }
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alert.create({
+      header,
+      message,
+      buttons: ["OK"]
+    })  
+
+    await alert.present();
   }
 
 }
