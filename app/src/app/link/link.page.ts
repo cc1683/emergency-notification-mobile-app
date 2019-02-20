ee068d4f13ec04c60d77192066004250f21bdcb3
luis <yongchun1683@gmail.com>
2019-02-20 16:41:37 +0800

    link user feature

--- app/src/app/link/link.page.ts
+++ app/src/app/link/link.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { UserService } from '../user.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-link',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinkPage implements OnInit {
 
-  constructor() { }
+  emailsCollection: AngularFirestoreCollection<any>
+  emails: Array<any> = []
+  linkUserEmail: string
+
+  constructor(public afAuth: AngularFireAuth, public afStore: AngularFirestore, public user: UserService) { 
+    this.emailsCollection = this.afStore.collection('users')
+    this.emailsCollection.snapshotChanges().forEach(a => {
+      a.forEach(item => {
+        const useremail = item.payload.doc.data().username
+        this.emails.push(useremail)
+      })
+    })
+  }
 
   ngOnInit() {
   }
 
+  linkUser(newInputEmail: string) {
+    for(let email of this.emails) {
+      if(newInputEmail === email) {
+        this.afStore.doc(`users/${this.user.getUid()}`).update({
+          usersList: firestore.FieldValue.arrayUnion({
+            newInputEmail
+          })
+        })
+      } 
+    }
+  }
 }
