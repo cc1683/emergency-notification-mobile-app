6ccb08ee061f660acac9340d9588ca653d882b37
luis <yongchun1683@gmail.com>
2019-02-21 20:28:53 +0800

    add cordova geolocation plugin

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { UserService } from '../user.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx'
 
 @Component({
   selector: 'app-home',
@@ -12,20 +13,30 @@ export class HomePage implements OnInit {
   lat: number
   lng: number
 
-  constructor(public afStore: AngularFirestore, public user: UserService) { }
+  constructor(public afStore: AngularFirestore, public user: UserService, public geolocation: Geolocation) { }
 
   ngOnInit() {
-    this.getUserLocation()
+    // this.getUserLocation()
+    this.geolocation.getCurrentPosition().then((postion) => {
+      this.lat = postion.coords.latitude
+      this.lng = postion.coords.longitude
+    })
   }
 
-  getUserLocation() {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.lat = position.coords.latitude
-        this.lng = position.coords.longitude
-      })
-    }
-  }
+  // getUserLocation() {
+  //   let option = {
+  //     enableHighAccuracy: true,
+  //     timeout: 5000,
+  //     maximumAge: 0
+  //   }
+
+  //   if(navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(position => {
+  //       this.lat = position.coords.latitude
+  //       this.lng = position.coords.longitude
+  //     })
+  //   }
+  // }
 
   updateLocation() {
     this.afStore.doc(`users/${this.user.getUid()}`).update({
