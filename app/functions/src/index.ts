8326dba2a800be1aa128d17274d43cf74311ca97
luis <yongchun1683@gmail.com>
2019-03-02 18:34:26 +0800

    msg can now be sent to another linked device

--- app/functions/src/index.ts
+++ app/functions/src/index.ts
@@ -1,23 +1,49 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 
-// import * as admin from 'firebase-admin';
-// import { async } from 'q';
-
-
-// admin.initializeApp(functions.config().firebase);
+import * as admin from 'firebase-admin';
+import { async } from 'q';
 
 
+admin.initializeApp(functions.config().firebase);
 
-// exports.emergencyNotification = functions.firestore
-//     .document('notifications/{notificationId}')
-//     .onCreate(async event => {
-//       const data = event.data().data
+export const emergencyNotif = functions.firestore
+    .document('users/{userID}/notifications/{notificationID}')
+    .onCreate(async event => {
+
+      const data = event.data();
+
+      const sender = data.sender
+      const msg = data.message
+
+      const payload = {
+        notification: {
+          title: `${sender} need your help!`,
+          body: msg 
+        }
+      }
+
+      const db = admin.firestore()
+      const deviceRef = db.collection('users').where('username', '==', sender)
+
+      const devices = await deviceRef.get()
+
+      let tokens = []
+
+      let receiverDevices = []
       
-//     })
+      devices.forEach(result => {
+          tokens = result.data().usersList
+          if(tokens.length > 0) {
+            for(var i=0; i<tokens.length; i++) {
+              let token = result.data().usersList[i].newToken
+              receiverDevices.push(token)
+            }
+          }
+      })
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+      return admin.messaging().sendToDevice(receiverDevices, payload).then(result => {
+        console.log('success')
+        receiverDevices = []
+      })
+
+    })
