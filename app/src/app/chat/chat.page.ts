cb2ba42c9dc5fc0b76a36612a81aad1f45b2e543
luis <yongchun1683@gmail.com>
2019-03-13 09:30:32 +0800

    brief UI for chat page

--- app/src/app/chat/chat.page.ts
+++ app/src/app/chat/chat.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { UserService } from '../user.service';
+import { Observable } from 'rxjs';
+import { firestore } from 'firebase';
 
 @Component({
   selector: 'app-chat',
@@ -10,12 +12,20 @@ import { UserService } from '../user.service';
 })
 export class ChatPage implements OnInit {
 
-  message: string
+  userData: Observable<any>
+
   constructor(public afAuth: AngularFireAuth,
               public afStore: AngularFirestore,
               public user: UserService
-              ) { }
+              ) {
+
+                const data = afStore.doc(`users/${user.getUid()}`)
+                this.userData = data.valueChanges()
+                
+              }
 
   ngOnInit() {
   }
+
+
 }
